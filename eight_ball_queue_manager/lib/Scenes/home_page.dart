8a63c737b4f3305891ef987767ddca0d551a9b17
renan vargas <renanspvargas@outlook.com>
2022-10-28 20:46:07 -0300

    changing main and created home page

---
+++ eight_ball_queue_manager/lib/Scenes/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:eight_ball_queue_manager/imports.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Adicionar jogador",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 25),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 45,
+              width: size.width * 0.35,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Adicionar a fila"),
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Divider(thickness: 5),
+            const SizedBox(height: 40),
+            const Text(
+              "Gerenciar fila",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 45,
+                  width: size.width * 0.35,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.redAccent,
+                      ),
+                    ),
+                    child: const Text("Jogador anterior"),
+                  ),
+                ),
+                const SizedBox(width: 40),
+                SizedBox(
+                  height: 45,
+                  width: size.width * 0.35,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.green,
+                      ),
+                    ),
+                    child: const Text("Próximo jogador"),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
