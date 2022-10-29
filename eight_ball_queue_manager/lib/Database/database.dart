b5a7a37416c7c10130e02b4c848cdd862e4dfff3
renan vargas <renanspvargas@outlook.com>
2022-10-28 22:06:36 -0300

    db basic configuration

--- eight_ball_queue_manager/lib/Database/database.dart
+++ eight_ball_queue_manager/lib/Database/database.dart
@@ -1,6 +1,33 @@
+import 'package:eight_ball_queue_manager/imports.dart';
+
 class DB {
   DB._();
   static final DB instance = DB._();
+  static Database? _database;
+
+  get database async {
+    if (_database != null) return _database;
+    return await _initDatabase();
+  }
+
+  _initDatabase() async {
+    sqfliteFfiInit();
+    String databasePath = await databaseFactoryFfi.getDatabasesPath();
+    String path = join(databasePath, 'queue.db');
+    print(path);
+    DatabaseFactory databaseFactory = databaseFactoryFfi;
+    return await databaseFactory.openDatabase(
+      path,
+      options: OpenDatabaseOptions(
+        onCreate: _onCreate,
+        version: 1,
+      ),
+    );
+  }
+
+  _onCreate(db, versao) async {
+    await db.execute(_players);
+  }
 
   String get _players => '''
     CREATE TABLE players (
