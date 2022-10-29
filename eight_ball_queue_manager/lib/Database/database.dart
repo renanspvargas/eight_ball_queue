fdbcafb7b54b2fb73903b214ca5d08e42cb91e97
renan vargas <renanspvargas@outlook.com>
2022-10-28 21:13:42 -0300

    creating db scheme

--- eight_ball_queue_manager/lib/Database/database.dart
+++ eight_ball_queue_manager/lib/Database/database.dart
@@ -1,4 +1,11 @@
 class DB {
   DB._();
   static final DB instance = DB._();
+
+  String get _players => '''
+    CREATE TABLE players (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT
+    )
+  ''';
 }
