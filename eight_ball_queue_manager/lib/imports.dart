e4cb7d49106a0dde95a6fdb1da19247bd44fd84d
renan vargas <renanspvargas@outlook.com>
2022-12-01 18:42:37 -0300

    adding paths to imports file

--- eight_ball_queue_manager/lib/imports.dart
+++ eight_ball_queue_manager/lib/imports.dart
@@ -8,3 +8,7 @@ export 'package:eight_ball_queue_manager/Scenes/home_page.dart';
 export 'package:path/path.dart';
 export 'package:sqflite/sqflite.dart';
 export 'package:sqflite_common_ffi/sqflite_ffi.dart';
+export 'package:eight_ball_queue_manager/Database/database.dart';
+
+//Models
+export 'package:eight_ball_queue_manager/Models/player_model.dart';
